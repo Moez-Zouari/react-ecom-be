f6a295ab9c991df5b917706272faf2ea76744f7e
moezzouari <moez@octoplus.tech>
2023-09-13 13:26:38 +0100

    Register And Login User

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/user")
+const bcrypt = require('bcrypt')
+// npm i jsonwebtoken bcrypt
+const jwt = require('jsonwebtoken')
 // npm install nodemailer
 const nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
@@ -78,6 +81,51 @@ router.get('/status/edit/', async (req, res) => {
     } catch (err) {
         return res.status(404).send({ success: false, message: err })
     }
-})
+});
 
+
+// se connecter
+router.post('/login', async (req, res) => {
+    try {
+        let { email, password } = req.body
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false, message: "All fields are required"
+            })
+        }
+        let user = await User.findOne({
+            email
+        }).select('+password').select('+isActive')
+        if (!user) {
+            return res.status(404).send({
+                success: false, message: "Account doesn't exists"
+            })
+        } else {
+            let isCorrectPassword = await bcrypt.compare(password, user.password)
+            if (isCorrectPassword) {
+                delete user._doc.password
+                if (!user.isActive) return res.status(200).send({
+                    success:
+                        false, message: 'Your account is inactive, Please contact your  administrator'
+                })
+                const token = jwt.sign({
+                    iduser:
+                        user._id, name: user.firstname, email: user.email, role: user.role
+                }, process.env.SECRET, {
+                    expiresIn: "1h",
+                })
+                return res.status(200).send({ success: true, user, token })
+            } else {
+                return res.status(404).send({
+                    success: false, message:
+                        "Please verify your credentials"
+                })
+            }
+        }
+    } catch (err) {
+        return res.status(404).send({
+            success: false, message: err.message
+        })
+    }
+});
 module.exports = router;
